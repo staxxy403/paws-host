00c07033a8b8502f1a6e7483c8d0eb63a323d4d8
furry <furry@alpine.my.domain>
2026-08-30 19:53:18 +0300

    add db functions for user

---
+++ database/logic/node.py
@@ -0,0 +1,68 @@
+import uuid
+from sqlalchemy import select, func
+from sqlalchemy.orm import selectinload, joinedload
+
+from shared.enums import VPSStatus
+
+from database.core import VPS, Node, IPAddress
+from database.core import async_session
+
+
+async def get_node(node_id: int) -> Node | None:
+    '''Get Node by ID with its relationships'''
+    stmt = (
+        select(Node)
+        .where(Node.id == node_id)
+        .options(
+            joinedload(Node.category),
+            joinedload(Node.location),
+        )
+        .options(
+            selectinload(Node.ip_pool),
+            selectinload(Node.vps_list),
+        )
+    )
+
+    async with async_session() as session:
+        return await session.scalar(stmt)
+
+
+async def get_node_used_ram(node_id: int) -> int:
+    '''Returns RAM used by Node (count only VPS with status ACTIVE and CREATING)'''
+    stmt = (
+        select(func.sum(VPS.ram_gb))
+        .where(
+            VPS.node_id == node_id,
+            VPS.status.in_([VPSStatus.ACTIVE, VPSStatus.CREATING])
+        )
+    )
+
+    async with async_session() as session:
+        used_ram = await session.scalar(stmt)
+
+        return used_ram or 0
+
+
+async def find_available_node(location_id: int, category_id: int, required_ram_gb: int) -> Node | None:
+    '''Returns Node with enough amount of resources to deploy a new VPS'''
+    stmt = (
+        select(Node)
+        .where(
+            Node.location_id == location_id,
+            Node.category_id == category_id,
+            Node.is_active == True,
+            Node.ip_pool.any(IPAddress.is_allocated == False)
+        )
+    )
+
+    async with async_session() as session:
+        nodes = await session.scalars(stmt)
+
+        for node in nodes:
+            used_ram = await get_node_used_ram(node.id)
+            free_ram = node.max_ram_gb - used_ram
+
+            if free_ram >= required_ram_gb:
+                return node
+
+        return None
---
+++ database/logic/user.py
@@ -0,0 +1,30 @@
+import uuid
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload
+
+from shared.enums import VPSStatus
+
+from database.core import VPS, Node, User
+from database.core import async_session
+
+
+async def get_user(user_id: int) -> User | None:
+    '''Get User by ID'''
+    async with async_session() as session:
+        return await session.get(User, user_id)
+
+
+async def get_user_by_tg_id(telegram_id: int) -> User | None:
+    '''Get User by Telegram ID'''
+    stmt = select(User).where(User.telegram_id == telegram_id)
+    async with async_session() as session:
+        return await session.scalar(stmt)
+
+
+async def create_user(telegram_id: int) -> User:
+    new_user = User(telegram_id=telegram_id)
+    async with async_session() as session:
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
