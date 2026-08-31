131b8be49648ea4679e9556e01a9bb977cd57f44
furry <furry@alpine.my.domain>
2026-08-31 18:58:04 +0300

    first routes for VPS

--- api/logic/vps.py
+++ api/logic/vps.py
@@ -1,6 +1,19 @@
-from shared.models.responses import success_response
+import uuid
+
 from database.logic import vps as vps_logic
+from shared.models import VPSListResponse
+from shared.models.responses import success_response
 
 
-async def get_list_vps_logic(user_id: int):
-    await vps_logic.get_list_by_user_id(user_id=user_id)
+async def get_list_vps_logic(user_id: uuid.UUID):
+    vps_list = await vps_logic.get_list_by_user_id(user_id=user_id)
+
+    return success_response(
+        VPSListResponse(
+            vps=vps_list
+        )
+    )
+
+
+async def buy_vps_logic():
+    ...
--- api/routes/vps.py
+++ api/routes/vps.py
@@ -4,17 +4,21 @@ from arq.connections import ArqRedis
 from api.dependencies import valid_user, valid_vps, get_arq_pool
 from database.core import VPS, User
 
+from api.logic.vps import get_list_vps_logic
+from shared.models.responses import success_response
+from shared.models import VPSRead
+
 router = APIRouter(prefix='/vps')
 
 
 @router.get('')
 async def api_get_vps_list(current_user: User = Depends(valid_user)):
-    ...
+    return await get_list_vps_logic(user_id=current_user.id)
 
 
 @router.get('/{vps_id}')
 async def api_get_vps(vps: VPS = Depends(valid_vps)):
-    ...
+    return success_response(VPSRead.model_validate(vps))
 
 
 @router.post('/buy')
